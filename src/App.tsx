02d3e1ecccf37f20cb92e630d5c5ddb68443fb96
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2026-01-02 15:16:29 +0000

    feat: refine styling for large screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black p-2">
+    <div className="flex items-center justify-center min-h-screen bg-black p-2 @container">
       <Calculator />
     </div>
   );
